04db480289d87f75011aa3f0575bd5a656d69de2
JToong <toongjosh@gmail.com>
2020-11-22 08:40:32 +0800

    seeded

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,9 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+
+  validates :description, presence: true 
+  # uniqueness [:cocktail_id, :ingredient_id]
+validates :ingredient, uniqueness: { scope: :cocktail,
+    message: "Cannot have same ingredient twice" }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,19 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
+puts "Cleaning database..."
+Ingredient.destroy_all
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "Tea")
+Ingredient.create(name: "Coffee liqeur")
+Ingredient.create(name: "Bitters")
+Ingredient.create(name: "Sugar")
+Ingredient.create(name: "Kahlua")
+Ingredient.create(name: "Tequila")
+
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
